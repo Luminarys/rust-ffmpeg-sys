4295869f0617ab41a767ed28fd8240a05e33a173
Luminarys <postmaster@gensok.io>
2017-08-12 17:32:58 -0700

    Add non static build options

--- build.rs
+++ build.rs
@@ -1,25 +1,32 @@
 use std::env;
 
 fn main() {
-    let target_dir = env::var("FFMPEG_DIR").unwrap();
-    let lib_dir = format!("{}/lib", target_dir);
+    let link_method;
+    let link_dir;
+    if let Ok(target_dir) = env::var("FFMPEG_DIR") {
+        link_method = "static";
+        link_dir = format!("{}/lib", target_dir);
+    } else {
+        link_method = "dylib";
+        link_dir = format!("/usr/lib");
+    }
 
-    println!("cargo:rustc-link-search=native={}", lib_dir);
+    println!("cargo:rustc-link-search={}", link_dir);
 
-    println!("cargo:rustc-link-lib=static=z");
+    println!("cargo:rustc-link-lib={}=z", link_method);
 
-    println!("cargo:rustc-link-lib=static=mp3lame");
-    println!("cargo:rustc-link-lib=static=opus");
-    println!("cargo:rustc-link-lib=static=ogg");
-    println!("cargo:rustc-link-lib=static=vorbis");
-    println!("cargo:rustc-link-lib=static=vorbisfile");
-    println!("cargo:rustc-link-lib=static=vorbisenc");
+    println!("cargo:rustc-link-lib={}=mp3lame", link_method);
+    println!("cargo:rustc-link-lib={}=opus", link_method);
+    println!("cargo:rustc-link-lib={}=ogg", link_method);
+    println!("cargo:rustc-link-lib={}=vorbis", link_method);
+    println!("cargo:rustc-link-lib={}=vorbisfile", link_method);
+    println!("cargo:rustc-link-lib={}=vorbisenc", link_method);
 
-    println!("cargo:rustc-link-lib=static=avcodec");
-    println!("cargo:rustc-link-lib=static=avutil");
-    println!("cargo:rustc-link-lib=static=avformat");
-    println!("cargo:rustc-link-lib=static=avfilter");
-    println!("cargo:rustc-link-lib=static=avdevice");
-    println!("cargo:rustc-link-lib=static=swresample");
-    println!("cargo:rustc-link-lib=static=swscale");
+    println!("cargo:rustc-link-lib={}=avcodec", link_method);
+    println!("cargo:rustc-link-lib={}=avutil", link_method);
+    println!("cargo:rustc-link-lib={}=avformat", link_method);
+    println!("cargo:rustc-link-lib={}=avfilter", link_method);
+    println!("cargo:rustc-link-lib={}=avdevice", link_method);
+    println!("cargo:rustc-link-lib={}=swresample", link_method);
+    println!("cargo:rustc-link-lib={}=swscale", link_method);
 }
